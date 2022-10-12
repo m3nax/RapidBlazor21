713ddebaab1ff5a33ae4b0a7b4872b385aeec758
Jason Taylor <jasongt@outlook.com>
2022-10-12 13:55:09 +1100

    🎉 New project

---
+++ src/WebUI/Client/Shared/FlexibleAuthorizeView.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using RapidBlazor21.WebUI.Shared.Authorization;
+
+namespace RapidBlazor21.WebUI.Client.Shared;
+
+public class FlexibleAuthorizeView : Microsoft.AspNetCore.Components.Authorization.AuthorizeView
+{
+    [Parameter]
+    public Permissions Permissions
+    {
+        get
+        {
+            return string.IsNullOrEmpty(Policy) ? Permissions.None : PolicyNameHelper.GetPermissionsFrom(Policy);
+        }
+        set
+        {
+            Policy = PolicyNameHelper.GeneratePolicyNameFor(value);
+        }
+    }
+}
